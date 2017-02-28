ca24f093bcc7b72e983f49f4a9a7689f967d3a77
HyunJong Park <phjwithyou@naver.com>
2017-02-28 22:54:58 +0900

    print ui
    Signed-off-by: HyunJong Park <phjwithyou@naver.com>

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 import pandas_datareader.data as web
 import datetime
+import matplotlib.pyplot as plt
 
 start = datetime.datetime(2016,1,19)
-end = datetime.datetime(2016,2,19)
-gs = web.DataReader("078930.ks", "yahoo", start, end)
-print(gs)
+end = datetime.datetime(2017,12,31)
+gs = web.DataReader("005930.ks", "yahoo", start, end)
+plt.plot(gs.index, gs['Close'])
+plt.title('TEST')
+plt.show()
