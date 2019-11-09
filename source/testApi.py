69f97dff90a3ba5ac0f9e2fb1724653bed499177
hyunjongpark <phjwithyou@naver.com>
2019-11-09 21:24:58 +0900

    add check trade in trade time
    Signed-off-by: hyunjongpark <phjwithyou@naver.com>

--- source/testApi.py
+++ source/testApi.py
@@ -45,24 +45,13 @@ class XASessionEvents:
 
 
 class XAQueryEvents:
-    query_state = STAND_BY
-
-    def OnReceiveData(self, code):
-        XAQueryEvents.query_state = RECEIVED
-
-    def OnReceiveMessage(self, error, nMessageCode, szMessage):
-        print(szMessage)
-
-
-class XAQueryEvents:
     상태 = False
 
     def OnReceiveData(self, szTrCode):
-        print("OnReceiveData : %s" % szTrCode)
         XAQueryEvents.상태 = True
 
-    def OnReceiveMessage(self, systemError, messageCode, message):
-        print("OnReceiveMessage : ", systemError, messageCode, message)
+    # def OnReceiveMessage(self, systemError, messageCode, message):
+    #     print("OnReceiveMessage : ", systemError, messageCode, message)
 
 
 class Trade():
@@ -84,14 +73,15 @@ class Trade():
             pythoncom.PumpWaitingMessages()
         XASessionEvents.login_state = STAND_BY
 
-    def check_realTime_stoks(self):
-        print('check_realTime_stoks')
+    def check_realTime_stock(self):
+        print('check_realTime_stock')
 
         today = datetime.date.today()
-        today_list = []
         getListTime = datetime.datetime(today.year, today.month, today.day, 8, 58, 0)
         startTime = datetime.datetime(today.year, today.month, today.day, 9, 0, 0)
         endTime = datetime.datetime(today.year, today.month, today.day, 15, 30, 0)
+
+        today_list = []
 
         while True:
             if datetime.datetime.now() < startTime:
@@ -99,7 +89,7 @@ class Trade():
                 sleep(5)  # 10 -> 1분
                 continue
 
-            if len(today_list) != 0 and datetime.datetime.now() < getListTime:
+            if len(today_list) == 0 and datetime.datetime.now() > getListTime:
                 today_list = self.get_top_trade_volume(field=1)
                 print(today_list)
 
@@ -116,27 +106,38 @@ class Trade():
                 if df is None:
                     print('Skip:[%s]' % (code))
                     continue
-                stockManager.register(code, df)
+
                 df.to_csv('log/%s/t1302_%s_%s.csv' % (TODAY, TODAY, code), mode='a', index=False, header=False)
+
+                stockManager.register(code, df)
+                trade = stockManager.get_stock_code(code).is_trade()
+                if trade == 'buy':
+                    print('BUY [%s][%s][%s]' % (code, df['시간'], df['종가']))
+                elif trade == 'sell_success':
+                    print('SELL SUCCESS [%s][%s][%s]' % (code, df['시간'], df['종가']))
+                elif trade == 'sell_failed':
+                    print('SELL FAILED [%s][%s][%s]' % (code, df['시간'], df['종가']))
+
+
+
 
     def get_top_trade_volume(self, field=1, day=0):
         sleep(1)
         inXAQuery = win32com.client.DispatchWithEvents("XA_DataSet.XAQuery", XAQueryEvents)
-        inXAQuery.LoadFromResFile("C:\\eBest\\xingAPI\\Res\\t1488.res")
-        inXAQuery.SetFieldData('t1488InBlock', 'gubun', 0, field)
-        # inXAQuery.SetFieldData('t1488InBlock', 'jnilgubun', 0, day)
+        inXAQuery.LoadFromResFile("C:\\eBest\\xingAPI\\Res\\t1489.res")
+        inXAQuery.SetFieldData('t1489InBlock', 'gubun', 0, field)
         inXAQuery.Request(0)
 
         while XAQueryEvents.상태 == False:
             pythoncom.PumpWaitingMessages()
+        XAQueryEvents.상태 = False
 
-        nCount = inXAQuery.GetBlockCount('t1488OutBlock1')
+        nCount = inXAQuery.GetBlockCount('t1489OutBlock1')
         result = []
         for i in range(nCount):
-            code = inXAQuery.GetFieldData('t1488OutBlock1', 'shcode', i)
+            code = inXAQuery.GetFieldData('t1489OutBlock1', 'shcode', i)
             result.append(code)
 
-        XAQueryEvents.상태 = False
         return result
 
     def t1302(self, 단축코드='', 작업구분='1', 시간='1', 건수='1'):
@@ -163,10 +164,11 @@ class Trade():
         while XAQueryEvents.상태 == False:
             pythoncom.PumpWaitingMessages()
 
+        XAQueryEvents.상태 = False
+
         result = []
         nCount = query.GetBlockCount(OUTBLOCK)
         if nCount == 0:
-            XAQueryEvents.상태 = False
             return (None, None)
 
         for i in range(nCount):
@@ -211,15 +213,13 @@ class Trade():
             result.append(lst)
 
         df1 = DataFrame(data=result,
-                        columns=[시간, 단축코드, 종가, 전일대비구분, 전일대비, 등락율, 체결강도, 매도체결수량, 매수체결수량, 순매수체결량, 매도체결건수, 매수체결건수, 순체결건수,
-                                 거래량, 시가, 고가, 저가, 체결량,
-                                 매도체결건수시간, 매수체결건수시간, 매도잔량, 매수잔량, 시간별매도체결량, 시간별매수체결량])
-
-        XAQueryEvents.상태 = False
+                        columns=['시간', '단축코드', '종가', '전일대비구분', '전일대비', '등락율', '체결강도', '매도체결수량', '매수체결수량', '순매수체결량', '매도체결건수', '매수체결건수', '순체결건수',
+                                 '거래량', '시가', '고가', '저가', '체결량',
+                                 '매도체결건수시간', '매수체결건수시간', '매도잔량', '매수잔량', '시간별매도체결량', '시간별매수체결량'])
 
         return (df, df1)
 
-    def file_test2(self):
+    def file_test(self):
         TODAY = '20191108'
         log_folder = ('log/%s' % (TODAY))
         if not os.path.exists(log_folder):
@@ -235,128 +235,23 @@ class Trade():
             for i in df.index:
                 code = df['단축코드'][i]
                 stockManager.register(code, df.iloc[i])
-                # stockManager.get_stock_code(code).is_buy_price()
-            # stockManager.get_stock_code(code).is_buy_price()
-            stockManager.all_print_stock()
+                trade = stockManager.get_stock_code(code).is_trade()
+                if trade == 'buy':
+                    print('BUY [%s][%s][%s]' %(code, df['시간'][i], df['종가'][i]))
+                elif trade == 'sell_success':
+                    print('SELL SUCCESS [%s][%s][%s]' %(code, df['시간'][i], df['종가'][i]))
+                elif trade == 'sell_failed':
+                    print('SELL FAILED [%s][%s][%s]' %(code, df['시간'][i], df['종가'][i]))
+
+            stockManager.get_stock_code(code).show_graph()
 
 
-    def file_test(self):
-        TODAY = '20191106'
-        log_folder = ('log/%s' % (TODAY))
-        if not os.path.exists(log_folder):
-            return
-        files = os.listdir(log_folder)
-
-        for file in files:
-            df = pd.read_csv('log/%s/%s' % (TODAY, file),
-                             names=['시간', '단축코드', '종가', '전일대비구분', '전일대비', '등락율', '체결강도', '매도체결수량', '매수체결수량', '순매수체결량',
-                                    '매도체결건수',
-                                    '매수체결건수', '순체결건수', '거래량', '시가', '고가', '저가', '체결량', '매도체결건수시간', '매수체결건수시간', '매도잔량',
-                                    '매수잔량', '시간별매도체결량', '시간별매수체결량'])
-            volume_list = []
-            buy_list = []
-            diff_buy_list = []
-            code = ''
-
-            test_buy_price_list = []
-            test_buy_index_list = []
-
-            is_buy = False
-            test_success_sell_price_list = []
-            test_success_sell_index_list = []
-
-            test_fail_sell_price_list = []
-            test_fail_sell_index_list = []
-
-            buy_limit_list = []
-            max_volume = 0
-
-            real_buy_percent = 50
-
-            for i in df.index:
-                if 0 == i:
-                    volume_list.append(0)
-                    buy_list.append(0)
-                    diff_buy_list.append(0)
-                    buy_limit_list.append(0)
-                    code = df['단축코드'][i]
-                    continue
-
-                diff_time = int(df['시간'][i]) - int(df['시간'][i - 1])
-                diff_volume = int(df['거래량'][i]) - int(df['거래량'][i - 1])
-                diff_buy_pre_limit = int(df['매수잔량'][i - 1]) - int(df['매도잔량'][i - 1])
-                diff_buy_limit = int(df['매수잔량'][i]) - int(df['매도잔량'][i])
-                diff_diff_buy = diff_buy_limit - diff_buy_pre_limit
-
-                volume_list.append(diff_volume)
-                buy_list.append(diff_buy_limit)
-                diff_buy_list.append(diff_diff_buy)
-
-                if diff_volume > max_volume:
-                    max_volume = diff_volume;
-
-                buy_limit_price = (diff_buy_limit * diff_volume) / 100000000
-                buy_limit_list.append(buy_limit_price)
-
-                print('[%s] [%s] [%s] [%s] [%s] [%s]' % (df['시간'][i], df['등락율'][i], diff_buy_limit, diff_diff_buy, diff_volume, diff_time))
-
-                if diff_buy_limit > 0 and diff_diff_buy > 0 and diff_volume >= max_volume / 2 and df['등락율'][i] > \
-                        df['등락율'][i - 1] and buy_limit_price > 2 and df['등락율'][i] > 0 and diff_buy_list[i] > \
-                        diff_buy_list[i - 1]:
-                    print('============== Buy')
-                    test_buy_index_list.append([i])
-                    test_buy_price_list.append(df['등락율'][i])
-                    real_buy_percent = float(df['등락율'][i])
-                    is_buy = True
-
-                if is_buy is True and float(df['등락율'][i]) > real_buy_percent + 1.0:
-                    print('============== Sell')
-                    test_success_sell_index_list.append([i])
-                    test_success_sell_price_list.append(df['등락율'][i])
-                    real_buy_percent = 50
-                    is_buy = False
-
-                if is_buy is True and float(df['등락율'][i]) < real_buy_percent - 1.0:
-                    print('============== Failed Sell')
-                    test_fail_sell_index_list.append([i])
-                    test_fail_sell_price_list.append(df['등락율'][i])
-                    real_buy_percent = 50
-                    is_buy = False
-
-            fig, axs = plt.subplots(5)
-
-            ax = axs[0]
-            ax.plot(df['등락율'])
-            ax.scatter(test_buy_index_list, test_buy_price_list, c='r')
-            ax.scatter(test_success_sell_index_list, test_success_sell_price_list, c='b')
-            ax.scatter(test_fail_sell_index_list, test_fail_sell_price_list, c='y')
-            ax.grid(True)
-            plt.title(code)
-
-            ax = axs[1]
-            ax.plot(volume_list)
-            ax.grid(True)
-
-            ax = axs[2]
-            ax.plot(buy_list)
-            ax.grid(True)
-
-            ax = axs[3]
-            ax.plot(diff_buy_list)
-            ax.grid(True)
-
-            ax = axs[4]
-            ax.plot(buy_limit_list)
-            ax.grid(True)
-
-            plt.show()
 
 
 if __name__ == "__main__":
     debug_mode = True
     Trade = Trade(debug=debug_mode)
     if debug_mode:
-        # Trade.file_test()
-        Trade.file_test2()
+        Trade.file_test()
     else:
-        Trade.check_realTime_stoks()
+        Trade.check_realTime_stock()
--- source/util/StockManager.py
+++ source/util/StockManager.py
@@ -8,138 +8,179 @@ import pandas as pd
 
 class StockCode():
     def __init__(self, code):
-        # print('StockCode[%s]' % (code))
         self.df = pd.DataFrame()
         self.code = code
 
+        self.거래량_차이_리스트 = []
+        self.현재_시간_매수_매도_차이_리스트 = []
+        self.이전시간_현재시간_매수_매도_차이_리스트 = []
+        self.거래량_차이_리스트.append(0)
+        self.현재_시간_매수_매도_차이_리스트.append(0)
+        self.이전시간_현재시간_매수_매도_차이_리스트.append(0)
+
+        self.buy_list = []
+        self.max_volume = 0
+        self.is_buy = False
+        self.real_buy_percent = 50
+
+        self.index = 0
+
     def register(self, df):
-        self.df.append(df)
+        self.df = self.df.append(df, ignore_index=True)
 
-    def print(self):
-        for i in self.df.index:
-            print(self.df.idoc[i])
+    def is_trade(self):
+        is_trade = ''
+        if self.index == 0:
+            self.index += 1
+            return
 
-    def is_buy_price(self):
-        volume_list = []
+
+        거래량차이 = int(self.df['거래량'][self.index]) - int(self.df['거래량'][self.index - 1])
+        이전_시간_매수_매도_차이 = int(self.df['매수잔량'][self.index - 1]) - int(self.df['매도잔량'][self.index - 1])
+        현재_시간_매수_매도_차이 = int(self.df['매수잔량'][self.index]) - int(self.df['매도잔량'][self.index])
+        이전시간_현재시간_매수_매도_차이 = 현재_시간_매수_매도_차이 - 이전_시간_매수_매도_차이
+
+        self.거래량_차이_리스트.append(거래량차이)
+        self.현재_시간_매수_매도_차이_리스트.append(현재_시간_매수_매도_차이)
+        self.이전시간_현재시간_매수_매도_차이_리스트.append(이전시간_현재시간_매수_매도_차이)
+
+        if 거래량차이 > self.max_volume:
+            self.max_volume = 거래량차이;
+
+        남은매수대금대금 = (현재_시간_매수_매도_차이 * 거래량차이) / 100000000
+
+        if 이전시간_현재시간_매수_매도_차이 > 0 \
+                and 현재_시간_매수_매도_차이 > 0 \
+                and 거래량차이 >= self.max_volume / 2 \
+                and self.df['등락율'][self.index] > self.df['등락율'][self.index - 1] and self.df['등락율'][self.index] > 0 \
+                and 남은매수대금대금 > 2 \
+                and self.이전시간_현재시간_매수_매도_차이_리스트[self.index] > self.이전시간_현재시간_매수_매도_차이_리스트[self.index - 1]:
+            # print('============== Buy')
+            self.buy_list.append([self.index])
+            self.real_buy_percent = float(self.df['등락율'][self.index])
+            self.is_buy = True
+            if self.index == len(self.df.index)-1:
+                is_trade = 'buy'
+
+        if self.is_buy is True and float(self.df['등락율'][self.index]) > self.real_buy_percent + 1.0:
+            # print('============== Sell')
+            self.real_buy_percent = 50
+            self.is_buy = False
+
+            if self.index == len(self.df.index)-1:
+                is_trade = 'sell_success'
+
+        if self.is_buy is True and float(self.df['등락율'][self.index]) < self.real_buy_percent - 1.0:
+            # print('============== Failed Sell')
+            self.real_buy_percent = 50
+            self.is_buy = False
+            if self.index == len(self.df.index)-1:
+                is_trade = 'sell_failed'
+
+        self.index += 1
+        return is_trade
+
+
+    def show_graph(self):
+        거래량_차이_리스트 = []
+        현재_시간_매수_매도_차이_리스트 = []
+        이전시간_현재시간_매수_매도_차이_리스트 = []
+
+        등략율_리스트 = []
         buy_list = []
-        diff_buy_list = []
 
-        test_buy_price_list = []
-        test_buy_index_list = []
-
-        is_buy = False
         test_success_sell_price_list = []
         test_success_sell_index_list = []
 
         test_fail_sell_price_list = []
         test_fail_sell_index_list = []
 
-        buy_limit_list = []
+        거래대금_리스트 = []
         max_volume = 0
-
+        is_buy = False
         real_buy_percent = 50
 
-        index = 0
-        for df in self.list:
-            # print(df)
-            if 0 == index:
-                volume_list.append(0)
-                buy_list.append(0)
-                diff_buy_list.append(0)
-                buy_limit_list.append(0)
-                index = index + 1
+        for i in self.df.index:
+            if 0 == i:
+                거래량_차이_리스트.append(0)
+                현재_시간_매수_매도_차이_리스트.append(0)
+                이전시간_현재시간_매수_매도_차이_리스트.append(0)
+                거래대금_리스트.append(0)
                 continue
 
-            diff_time = int(df['시간'][index]) - int(df['시간'][index - 1])
-            diff_volume = int(df['거래량'][index]) - int(df['거래량'][index - 1])
-            diff_buy_pre_limit = int(df['매수잔량'][index - 1]) - int(df['매도잔량'][index - 1])
-            diff_buy_limit = int(df['매수잔량'][index]) - int(df['매도잔량'][index])
-            diff_diff_buy = diff_buy_limit - diff_buy_pre_limit
+            시간차이 = int(self.df['시간'][i]) - int(self.df['시간'][i - 1])
+            거래량차이 = int(self.df['거래량'][i]) - int(self.df['거래량'][i - 1])
+            이전_시간_매수_매도_차이 = int(self.df['매수잔량'][i - 1]) - int(self.df['매도잔량'][i - 1])
+            현재_시간_매수_매도_차이 = int(self.df['매수잔량'][i]) - int(self.df['매도잔량'][i])
+            이전시간_현재시간_매수_매도_차이 = 현재_시간_매수_매도_차이 - 이전_시간_매수_매도_차이
 
-            volume_list.append(diff_volume)
-            buy_list.append(diff_buy_limit)
-            diff_buy_list.append(diff_diff_buy)
+            거래량_차이_리스트.append(거래량차이)
+            현재_시간_매수_매도_차이_리스트.append(현재_시간_매수_매도_차이)
+            이전시간_현재시간_매수_매도_차이_리스트.append(이전시간_현재시간_매수_매도_차이)
 
-            if diff_volume > max_volume:
-                max_volume = diff_volume;
+            if 거래량차이 > max_volume:
+                max_volume = 거래량차이;
 
-            buy_limit_price = (diff_buy_limit * diff_volume) / 100000000
-            buy_limit_list.append(buy_limit_price)
+            남은매수대금대금 = (현재_시간_매수_매도_차이 * 거래량차이) / 100000000
+            거래대금_리스트.append(남은매수대금대금)
 
             print('[%s] [%s] [%s] [%s] [%s] [%s]' % (
-                df['시간'][index], df['등락율'][index], diff_buy_limit, diff_diff_buy, diff_volume, diff_time))
+            self.df['시간'][i], self.df['등락율'][i], 현재_시간_매수_매도_차이, 이전시간_현재시간_매수_매도_차이, 거래량차이, 시간차이))
 
-            if diff_buy_limit > 0 and diff_diff_buy > 0 and diff_volume >= max_volume / 2 and df['등락율'][index] > \
-                    df['등락율'][index - 1] and buy_limit_price > 2 and df['등락율'][index] > 0 and diff_buy_list[index] > \
-                    diff_buy_list[index - 1]:
+            if 이전시간_현재시간_매수_매도_차이 > 0 \
+                    and 현재_시간_매수_매도_차이 > 0 \
+                    and 거래량차이 >= max_volume / 2 \
+                    and self.df['등락율'][i] > self.df['등락율'][i - 1] and self.df['등락율'][i] > 0 \
+                    and 남은매수대금대금 > 2 \
+                    and 이전시간_현재시간_매수_매도_차이_리스트[i] > 이전시간_현재시간_매수_매도_차이_리스트[i - 1]:
                 print('============== Buy')
-                test_buy_index_list.append([index])
-                test_buy_price_list.append(df['등락율'][index])
-                real_buy_percent = float(df['등락율'][index])
+                buy_list.append([i])
+                등략율_리스트.append(self.df['등락율'][i])
+                real_buy_percent = float(self.df['등락율'][i])
                 is_buy = True
 
-            if is_buy is True and float(df['등락율'][index]) > real_buy_percent + 1.0:
+            if is_buy is True and float(self.df['등락율'][i]) > real_buy_percent + 1.0:
                 print('============== Sell')
-                test_success_sell_index_list.append([index])
-                test_success_sell_price_list.append(df['등락율'][index])
+                test_success_sell_index_list.append([i])
+                test_success_sell_price_list.append(self.df['등락율'][i])
                 real_buy_percent = 50
                 is_buy = False
 
-            if is_buy is True and float(df['등락율'][index]) < real_buy_percent - 1.0:
+            if is_buy is True and float(self.df['등락율'][i]) < real_buy_percent - 1.0:
                 print('============== Failed Sell')
-                test_fail_sell_index_list.append([index])
-                test_fail_sell_price_list.append(df['등락율'][index])
+                test_fail_sell_index_list.append([i])
+                test_fail_sell_price_list.append(self.df['등락율'][i])
                 real_buy_percent = 50
                 is_buy = False
 
-            index = index + 1
+        fig, axs = plt.subplots(5)
 
-
-
-    def show_graph(self):
-        fig, axs = plt.subplots(4)
         ax = axs[0]
-        # Solar = [float(line[1]) for line in I020]
-        ax.plot([float(stock.diff)for stock in self.list])
-        # ax.plot([float(stock.price) for stock in self.list])
-        # plt.gcf().autofmt_xdate()
+        ax.plot(self.df['등락율'])
+        ax.scatter(buy_list, 등략율_리스트, c='r')
+        ax.scatter(test_success_sell_index_list, test_success_sell_price_list, c='b')
+        ax.scatter(test_fail_sell_index_list, test_fail_sell_price_list, c='y')
         ax.grid(True)
-
-
-        # ax2 = ax.twinx()
-        # ax2.plot(get_foreigner_info(code, start, end), 'r')
-
-        # ax = axs[1]
-        # ax.plot([float(stock.volumediff) for stock in self.list])
-        # ax.grid(True)
 
         ax = axs[1]
-        ax.plot([float(stock.dvol1) for stock in self.list], 'b')
-        ax.plot([float(stock.svol1) for stock in self.list], 'r')
+        ax.plot(거래량_차이_리스트)
         ax.grid(True)
-        #
+
         ax = axs[2]
-        ax.plot([float(stock.dcha1)  for stock in self.list], 'b')
-        ax.plot([float(stock.scha1)  for stock in self.list], 'r')
+        ax.plot(현재_시간_매수_매도_차이_리스트)
         ax.grid(True)
-        # #
+
         ax = axs[3]
-        ax.plot([float(stock.ddiff1)  for stock in self.list], 'b')
-        ax.plot([float(stock.sdiff1)  for stock in self.list], 'r')
+        ax.plot(이전시간_현재시간_매수_매도_차이_리스트)
         ax.grid(True)
 
-        # ax = axs[5]
-        # ax.plot([float(stock.scha1)  for stock in self.list], 'y')
-        # ax.plot([float(stock.scha2)  for stock in self.list], 'r')
-        # ax.grid(True)
+        ax = axs[4]
+        ax.plot(거래대금_리스트)
+        ax.grid(True)
 
-        # ax = axs[6]
-        # ax.plot([float(stock.fwdvl) for stock in self.list], 'b')
-        # ax.plot([float(stock.fwsvl) for stock in self.list], 'r')
-        # ax.grid(True)
-
-
+        plt.title(self.code)
         plt.show()
+
 
 class StockManager:
     def __init__(self):
@@ -160,9 +201,7 @@ class StockManager:
     def all_print_stock(self):
         for stockCode in self.stocks.keys():
             code = self.stocks.get(stockCode)
-            code.print()
-            # code.show_graph()
-            # self.stocks.get(stockCode)
+            code.show_graph()
 
 
 
